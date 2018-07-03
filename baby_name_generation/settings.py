37f77fb9b5185eb45343cd6b8c9eef297aadab46
frankhart2017 <sdharchou@gmail.com>
2018-07-03 21:54:08 +0530

    Django deployment

--- baby_name_generation/settings.py
+++ baby_name_generation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%#(jukhce#73nn!$tr)6y#*)z@6db)v-h4#u!$m_zlkj7lc@mx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
